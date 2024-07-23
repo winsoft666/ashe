84c3ff03a4568905a6e4bc79528dc10b57c69e3f
winsoft666 <winsoft666@outlook.com>
2024-07-23 17:51:45 +0800

    Remove EASTL

--- src/eastl.cpp
+++
@@ -1,5 +0,0 @@
-#include "ashe/eastl.h"
-
-void* __cdecl operator new[](size_t size, const char* name, int flags, unsigned debugFlags, const char* file, int line) {
-    return new uint8_t[size];
-}
