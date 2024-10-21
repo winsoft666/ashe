9822f5de4e4c76827f4a6a94271362a78c73ca63
winsoft666 <winsoft666@outlook.com>
2024-10-21 15:14:49 +0800

    更新CMake

--- test/test.cpp
+++ test/test.cpp
@@ -18,7 +18,3 @@
 ******************************************************************************/
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
-int main(int argc, char* argv[]) {
-    return Catch::Session().run(argc, argv);
-}
